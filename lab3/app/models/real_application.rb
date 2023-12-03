adc786c66f10b1fd2b27aa19806f9d269c00bc18
wocqcm2 <no.1fuze@gmail.com>
2023-12-03 14:58:43 -0500

    Second feature finihsed

--- lab3/app/models/real_application.rb
+++ lab3/app/models/real_application.rb
@@ -1,3 +1,4 @@
 class RealApplication < ApplicationRecord
     validates :status, inclusion: { in: %w[pending approved denied] }
+    validates :time_intrested, inclusion: { in: %w[morning afternoon evening] }
 end
